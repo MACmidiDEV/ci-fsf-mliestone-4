c078f96758c5256af659c01299ae19e483784a5e
MACmidiDEV <macmidi.webdesigns@gmail.com>
2020-06-30 22:53:29 +0000

    ci-fsf-mile-4 | productsApp - dataBase

--- ci_fsf_milestone_4/settings.py
+++ ci_fsf_milestone_4/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -144,4 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
